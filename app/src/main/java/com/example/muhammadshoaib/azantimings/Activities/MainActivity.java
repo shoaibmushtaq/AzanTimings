5da6b3e01f3609ddb3acda3c6a4609f31e5886d7
Muhammad Shoaib <shoaibmushtaq2@gmail.com>
2019-05-13 10:53:08 +0500

    Initial commit

---
+++ app/src/main/java/com/example/muhammadshoaib/azantimings/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.muhammadshoaib.azantimings.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.muhammadshoaib.azantimings.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
